8598be27899365dcb8fbd689132492bbdfbb9c8b
Gurjog Singh <gurjog@ualberta.ca>
2022-01-25 15:59:15 -0700

    v2

--- README.md
+++ README.md
@@ -14,13 +14,16 @@ Generally everything is LICENSE'D under the Apache 2 license by Abram Hindle.
 
 server.py contains contributions from:
 
+* Gurjog Singh
 * Abram Hindle
 * Eddie Antonio Santos
 * Jackson Z Chang
 * Mandy Meindersma 
 
 But the server.py example is derived from the python documentation
-examples thus some of the code is Copyright © 2001-2013 Python
+examples thus some of the code is Copyright © 2001-2022 Python
 Software Foundation; All Rights Reserved under the PSF license (GPL
 compatible) http://docs.python.org/2/library/socketserver.html
+
+
 
--- server.py
+++ server.py
@@ -1,12 +1,11 @@
 #  coding: utf-8 
 from email.charset import BASE64
 from encodings import utf_8
-import socketserver, time, sys, socket, http, datetime, os, mimetypes
+import socketserver, time, sys, socket, datetime, os, mimetypes
 import pathlib
 from pathlib import Path
-from http import HTTPStatus
 
-# Copyright 2013 Abram Hindle, Eddie Antonio Santos
+# Copyright 2022 Gurjog Singh, 2013 Abram Hindle, Eddie Antonio Santos
 # 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -31,105 +30,104 @@ from http import HTTPStatus
 
 # try: curl -v -X GET http://127.0.0.1:8080/
 
-HOST, PORT, BUFFER_SIZE = "127.0.0.1", 8080, 1024
+HOST, PORT, BUFFER_SIZE = "127.0.0.1", 8080, 1
 BASEURL = "http://127.0.0.1:8080"
 
 class MyWebServer(socketserver.BaseRequestHandler):
     
     def handle(self):
+        #handles the request
        
-        self.data = self.request.recv(BUFFER_SIZE).strip()
+        self.data = b""
+        while True:
+            if self.data.find(b"\r\n\r\n") != -1:
+                break
+            data = self.request.recv(BUFFER_SIZE)
+            self.data += data
+            
+        self.data.strip()
+        #self.data = self.request.recv(BUFFER_SIZE).strip()
+  
 
         self.breaking_request()
         self.client_address
-        #print ("Got a request of: %s\n" % self.data)
 
         self.sending_to_terminal()
 
-       
-
-
     def sending_to_terminal(self):
-        #how to print current datetime https://www.w3resource.com/python-exercises/python-basic-exercise-3.php
-        now = datetime.datetime.now()
-        datetime_string = now.strftime("%d/%b/%Y %H:%M:%S")
+        #sends messages back to terminal running server
+
+        #how to print current datetime https://docs.python.org/3/library/time.html#time.struct_time
+
+        datetime_string = time.strftime("%a, %d %b %Y %H:%M:%S GMT", time.gmtime(time.time()))
 
         if self.code == 404:
             print("{0} - - [{1}] code: 404, message: File Not Found".format(self.client_address[0], datetime_string))
-            print("{0} - - [{4}] \"{1} {2} {3}\" {5} -".format(self.client_address[0], self.method, self.path, self.protocol, datetime_string, self.code))## look at what the dash is
+            print("{0} - - [{4}] \"{1} {2} {3}\" {5} -".format(self.client_address[0], self.method, self.origpath, self.protocol, datetime_string, self.code))## look at what the dash is
         elif self.code == 405:
             print("{0} - - [{1}] code: 405, message: Method Not Allowed".format(self.client_address[0], datetime_string))
+        elif self.code == 400:
+            print("{0} - - [{1}] code: 400, message: Bad Request".format(self.client_address[0], datetime_string))
         else:
-            print("{0} - - [{4}] \"{1} {2} {3}\" {5} -".format(self.client_address[0], self.method, self.path, self.protocol, datetime_string, self.code))## look at what the dash is
+            print("{0} - - [{4}] \"{1} {2} {3}\" {5} -".format(self.client_address[0], self.method, self.origpath, self.protocol, datetime_string, self.code))## look at what the dash is
         
 
 
     def breaking_request(self):
-        #self.method = "GET"
-        #self.protocol = "HTTP/1.1"
-        #self.path = "/"
+        #breaks down request recieved from client
+
         self.code = 000 #default
-        data_str = str(self.data,'utf_8')
+        data_str = str(self.data, 'utf-8')
+        
         
         stripped_request_header = data_str.split('\r\n', 1)[0]
         stripped_request_header = stripped_request_header.split()
         self.method = stripped_request_header[0]
         self.path = stripped_request_header[1]
+
+        self.origpath = self.path
         self.protocol = stripped_request_header[2]
+        
+        if self.protocol != "HTTP/1.1":
+            self.code = 400
+            self.bad_request_error()
+            return
 
         if self.method != "GET":
             self.code = 405
             self.request.sendall(bytearray("HTTP/1.1 405 Method Not Allowed text/html\r\n", 'utf-8'))
             self.request.sendall(bytearray("Allow: GET\r\n\r\n", 'utf-8'))
-            #self.request.close()
+
             return
         
         self.process_data()
         
     def process_data(self):
-        #get path and use path to send file 
+        #get and resolve path and use path to send file 
         #has to be /www/path
-        default_path = str(os.getcwd()) + "/www"
+        self.default_path = str(os.getcwd()) + "/www"
+        self.updated_path = None
         
-        """ if '..' in self.path:
-            print(self.path)
-            self.path = os.path.abspath(self.path)
-            print(os.path.abspath(self.path)) """
-            
-            #https://docs.python.org/3/library/os.path.html#os.path.abspath 
-
-
-        """
-        so whats happening.
-        since abspath function gets abs path, it removes the / at the end of the path after it is processed.
-        now some our paths (as per the requirements) need this / for different purposes
-        depending on whether / is in the orig path or not, / is then added on to the processed abs path
-        
-        """
-
         if self.path[-1] != '/':
-            print(self.path)
+            
             self.path = os.path.abspath(self.path)
-            print(os.path.abspath(self.path))
+            
             #https://docs.python.org/3/library/os.path.html#os.path.abspath
-            print("############# orig path: ", self.path)
             result = self.path.split(".")
-            print("############# result: ", result)
             if len(result) == 1:
                 self.code = 301
                 self.path = self.path + '/'
-                print("########## final path: ", self.path)
-
 
   
         if self.path[-1] == '/':
-            print(self.path)
+            
             self.path = os.path.abspath(self.path) + '/'
-            print(os.path.abspath(self.path))
+            
             #https://docs.python.org/3/library/os.path.html#os.path.abspath
             try:
-                f = open(default_path + self.path + "index.html")
-                self.content_type = mimetypes.guess_type(default_path + self.path + "index.html")
+                self.updated_path = self.default_path + self.path + "index.html"
+                f = open(self.updated_path)
+                self.content_type = mimetypes.guess_type(self.updated_path)
                 if self.code != 301:
                     self.code = 200
             except:
@@ -138,13 +136,12 @@ class MyWebServer(socketserver.BaseRequestHandler):
                     self.page_not_found_error()
                     return
                 else:
-                    return
-                #self.request.sendall(bytearray("Path Not Found\r\n", 'utf-8'))
-                
+                    return            
         else:
             try:
-                f = open(default_path + self.path)
-                self.content_type = mimetypes.guess_type(default_path + self.path)
+                self.updated_path = self.default_path + self.path
+                f = open(self.updated_path)
+                self.content_type = mimetypes.guess_type(self.updated_path)
                 self.code = 200
                 
             except:
@@ -154,8 +151,6 @@ class MyWebServer(socketserver.BaseRequestHandler):
                     return
                 else:
                     return
-                #self.request.sendall(bytearray("Path Not Found\r\n", 'utf-8'))
-        
         
         output_data = f.read()
         self.file_length = len(output_data)
@@ -167,86 +162,78 @@ class MyWebServer(socketserver.BaseRequestHandler):
     
         self.message = None
         if self.code == 200:
-            #< HTTP/1.0 200 OK
-            #< Server: SimpleHTTP/0.6 Python/3.7.3
-            #< Date: Thu, 20 Jan 2022 23:34:25 GMT
-            #< Content-type: text/html
-            #< Content-Length: 470
-             #< Last-Modified: Thu, 20 Jan 2022 00:24:14 GMT
             self.message = "OK"
             self.request.send(bytearray("{0} {1} {2}\r\n".format(self.protocol, self.code, self.message), 'utf-8'))
-            self.request.send(bytearray("Server: \r\n", 'utf-8'))
-            now = datetime.datetime.now()
-            datetime_string = now.strftime("%a, %d %b %Y %H:%M:%S")
+            self.request.send(bytearray("Server: Gurjog's Server\r\n", 'utf-8'))
+        
+            datetime_string = time.strftime("%a, %d %b %Y %H:%M:%S GMT", time.gmtime(time.time()))
             self.request.send(bytearray("Date: {0}\r\n".format(datetime_string), 'utf-8'))
             self.request.send(bytearray("Connection: close \r\n", 'utf-8'))
             self.request.send(bytearray("Content-type: {0}\r\n".format(self.content_type[0]), 'utf-8'))
             self.request.send(bytearray("Content-Length: {0}\r\n".format(self.file_length), 'utf-8'))
-            self.request.send(bytearray("Last-Modified: \r\n\r\n", 'utf-8'))
+            #https://www.askpython.com/python/examples/convert-seconds-hours-minutes
+            #https://www.kite.com/python/answers/how-to-get-the-last-modified-time-of-a-file-in-python
+            lastModified = time.strftime("%a, %d %b %Y %H:%M:%S GMT",(time.gmtime(os.path.getmtime(self.updated_path))))
+            self.request.send(bytearray("Last-Modified: {0}\r\n\r\n".format(lastModified), 'utf-8'))
         
             
         elif self.code == 301:
-        #< HTTP/1.0 301 Moved Permanently
-        #< Server: SimpleHTTP/0.6 Python/3.7.3
-        #< Date: Fri, 21 Jan 2022 05:21:33 GMT
-        #< Location: /www/deep/
-
+ 
             self.message = "Moved Permanently"
             self.request.send(bytearray("{0} {1} {2}\r\n".format(self.protocol, self.code, self.message), 'utf-8'))
-            self.request.send(bytearray("Server: \r\n", 'utf-8'))
-            now = datetime.datetime.now()
-            datetime_string = now.strftime("%a, %d %b %Y %H:%M:%S")
+            self.request.send(bytearray("Server: Gurjog's server\r\n", 'utf-8'))
+        
+            datetime_string = time.strftime("%a, %d %b %Y %H:%M:%S GMT", time.gmtime(time.time()))
             self.request.send(bytearray("Date: {0}\r\n".format(datetime_string), 'utf-8'))
             self.request.send(bytearray("Connection: close \r\n", 'utf-8'))
             self.request.send(bytearray("Content-type: {0}\r\n".format(self.content_type[0]), 'utf-8'))
             self.request.send(bytearray("Content-Length: {0}\r\n".format(self.file_length), 'utf-8'))
-            self.request.send(bytearray("Last-Modified: \r\n", 'utf-8'))
+            #https://www.askpython.com/python/examples/convert-seconds-hours-minutes
+            #https://www.kite.com/python/answers/how-to-get-the-last-modified-time-of-a-file-in-python
+            lastModified = time.strftime("%a, %d %b %Y %H:%M:%S GMT",(time.gmtime(os.path.getmtime(self.updated_path))))
+            self.request.send(bytearray("Last-Modified: {0}\r\n".format(lastModified), 'utf-8'))
             self.request.send(bytearray("Location: {0}\r\n\r\n".format(BASEURL + self.path), 'utf-8'))
 
             
-  
+    def bad_request_error(self):
+        #format stolen from https://uofa-cmput404.github.io/cmput404-slides/04-HTTP.html#/20
+            html_400_code = "<html>\r\n"\
+                "<head><title>400 BAD REQUEST</title></head>\r\n"\
+                "<body bgcolor=\"white\">\r\n"\
+                "<center><h1>400</h1></center\r\n>"\
+                "<hr><center>BAD REQUEST</center>\r\n"\
+                "</body>\r\n"\
+                "</html>\r\n"
+               
+            self.message = "Bad Request"
+            self.request.send(bytearray("{0} {1} {2}\r\n".format('HTTP/1.1', self.code, self.message), 'utf-8'))
+            self.request.send(bytearray("Server: Gurjog's Server\r\n", 'utf-8'))
+            datetime_string = time.strftime("%a, %d %b %Y %H:%M:%S GMT", time.gmtime(time.time()))
+           
+
+            self.request.send(bytearray("Date: {0}\r\n".format(datetime_string), 'utf-8'))
+            self.request.send(bytearray("Connection: close \r\n", 'utf-8'))
+            self.request.send(bytearray("Content-type: text/html;charset=utf-8\r\n", 'utf-8'))
+            self.request.send(bytearray("Content-Length: {0}\r\n\r\n".format(len(html_400_code)), 'utf-8'))
+            self.request.send(bytearray(html_400_code, 'utf-8'))
         
     def page_not_found_error(self):
-        #< HTTP/1.0 404 File not found
-        #< Server: SimpleHTTP/0.6 Python/3.7.3
-        #< Date: Fri, 21 Jan 2022 05:19:44 GMT
-        #< Connection: close
-        #< Content-Type: text/html;charset=utf-8
-        #< Content-Length: 469
-        #< 
-        #<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN"
-                #"http://www.w3.org/TR/html4/strict.dtd">
-        #<html>
-            #<head>
-                #<meta http-equiv="Content-Type" content="text/html;charset=utf-8">
-                #<title>Error response</title>
-            #</head>
-            #<body>
-                #<h1>Error response</h1>
-                #<p>Error code: 404</p>
-                #<p>Message: File not found.</p>
-                #<p>Error code explanation: HTTPStatus.NOT_FOUND - Nothing matches the given URI.</p>
-            #</body>
-        #</html>
-            html_404_code = "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01//EN\"\r\n" \
-                "\t\t\"http://www.w3.org/TR/html4/strict.dtd\">\r\n"\
-                "<html>\r\n"\
-                "\t<head>\r\n"\
-                    "\t\t<meta http-equiv=\"Content-Type\" content=\"text/html;charset=utf-8\">\r\n"\
-                    "\t\t<title>Error response</title>\r\n"\
-                "\t</head>\r\n"\
-                "\t<body>\r\n"\
-                    "\t\t<h1>Error response</h1>\r\n"\
-                    "\t\t<p>Error code: 404</p>\r\n"\
-                    "\t\t<p>Message: File not found.</p>\r\n"\
-                    "\t\t<p>Error code explanation: 404 - Nothing matches the given URI.</p>\r\n"\
-                "\t</body>\r\n"\
-            "</html>\r\n"
+
+            #format stolen from https://uofa-cmput404.github.io/cmput404-slides/04-HTTP.html#/20
+            html_404_code = "<html>\r\n"\
+                "<head><title>404 File Not Found</title></head>\r\n"\
+                "<body bgcolor=\"white\">\r\n"\
+                "<center><h1>404</h1></center\r\n>"\
+                "<hr><center>File Not Found</center>\r\n"\
+                "</body>\r\n"\
+                "</html>\r\n"
+               
             self.message = "File not found"
             self.request.send(bytearray("{0} {1} {2}\r\n".format(self.protocol, self.code, self.message), 'utf-8'))
-            self.request.send(bytearray("Server: \r\n", 'utf-8'))
-            now = datetime.datetime.now()
-            datetime_string = now.strftime("%a, %d %b %Y %H:%M:%S")
+            self.request.send(bytearray("Server: Gurjog's Server\r\n", 'utf-8'))
+            datetime_string = time.strftime("%a, %d %b %Y %H:%M:%S GMT", time.gmtime(time.time()))
+           
+
             self.request.send(bytearray("Date: {0}\r\n".format(datetime_string), 'utf-8'))
             self.request.send(bytearray("Connection: close \r\n", 'utf-8'))
             self.request.send(bytearray("Content-type: text/html;charset=utf-8\r\n", 'utf-8'))
@@ -264,16 +251,11 @@ if __name__ == "__main__":
     with socketserver.TCPServer((HOST, PORT), MyWebServer) as server:
         print("Serving HTTP on {0} port {1} (http://{0}:{1}/) ...".format(HOST, PORT))
     
-        #recieved_request_socket, recieved_request_addr = server.handle_request()
-        #print("Connected to ", recieved_request_addr)
-        #recieved_data = recieved_request_socket.recv
-        #server.socket.close()
-
         # Activate the server; this will keep running until you
         # interrupt the program with Ctrl-C
         server.serve_forever()
 
             
-
+#https://docs.python.org/3/library/socketserver.html#socketserver.BaseServer.finish_request
 
 
