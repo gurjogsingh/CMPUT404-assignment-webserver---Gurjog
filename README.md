0f53859afdf8ee898293b6902ec9610864cb84c9
Gurjog Singh <gurjog@ualberta.ca>
2022-01-26 20:00:42 -0700

    final

--- README.md
+++ README.md
@@ -25,5 +25,13 @@ examples thus some of the code is Copyright © 2001-2022 Python
 Software Foundation; All Rights Reserved under the PSF license (GPL
 compatible) http://docs.python.org/2/library/socketserver.html
 
+Following sources were used:
+- How to print current GMT time https://www.askpython.com/python/examples/convert-seconds-hours-minutes (Copyright 2022 AskPython )
+- How to get absolute path from https://docs.python.org/3/library/os.path.html#os.path.abspath (Copyright 2001-2022, Python Software Foundation)
+- How to use mimetype from https://docs.python.org/3/library/mimetypes.html( Copyright 2001-2022, Python Software Foundation)
+- HTML format borrowed from https://uofa-cmput404.github.io/cmput404-slides/04-HTTP.html#/20 (Copyright 2014 Abram Hindle, Copyright 2019 Hazel Victoria Campbell and contributors)
+- How to get last modified time https://www.kite.com/python/answers/how-to-get-the-last-modified-time-of-a-file-in-python (Copyright 2022 Kite)
+
+
 
 
--- server.py
+++ server.py
@@ -1,9 +1,5 @@
 #  coding: utf-8 
-from email.charset import BASE64
-from encodings import utf_8
-import socketserver, time, sys, socket, datetime, os, mimetypes
-import pathlib
-from pathlib import Path
+import socketserver, time, os, mimetypes
 
 # Copyright 2022 Gurjog Singh, 2013 Abram Hindle, Eddie Antonio Santos
 # 
@@ -38,32 +34,27 @@ class MyWebServer(socketserver.BaseRequestHandler):
     def handle(self):
         #handles the request
        
-       #STOLE THIS CODE FROM LAB 2
+       #Used code from Lab 2 client.py (line 58-63) 
         self.data = b""
         while True:
-            #print(self.data)
             if self.data.find(b"\r\n\r\n") != -1:
                 break
             data = self.request.recv(BUFFER_SIZE)
             self.data += data
             if self.data == b"":
                 return
-
          
         self.data.strip()
         #self.data = self.request.recv(BUFFER_SIZE).strip()
   
-
         self.breaking_request()
         self.client_address
-
         self.sending_to_terminal()
 
     def sending_to_terminal(self):
         #sends messages back to terminal running server
 
-        #how to print current datetime https://docs.python.org/3/library/time.html#time.struct_time
-
+        #how to print current GMT time https://www.askpython.com/python/examples/convert-seconds-hours-minutes (Copyright 2022 AskPython )
         datetime_string = time.strftime("%a, %d %b %Y %H:%M:%S GMT", time.gmtime(time.time()))
 
         if self.code == 404:
@@ -83,8 +74,7 @@ class MyWebServer(socketserver.BaseRequestHandler):
 
         self.code = 000 #default
         data_str = str(self.data, 'utf-8')
-        
-        
+    
         stripped_request_header = data_str.split('\r\n', 1)[0]
         stripped_request_header = stripped_request_header.split()
         self.method = stripped_request_header[0]
@@ -114,10 +104,9 @@ class MyWebServer(socketserver.BaseRequestHandler):
         self.updated_path = None
         
         if self.path[-1] != '/':
-            
+            #how to get absolute path from https://docs.python.org/3/library/os.path.html#os.path.abspath (Copyright 2001-2022, Python Software Foundation)
             self.path = os.path.abspath(self.path)
-            
-            #https://docs.python.org/3/library/os.path.html#os.path.abspath
+        
             result = self.path.split(".")
             if len(result) == 1:
                 self.code = 301
@@ -125,13 +114,13 @@ class MyWebServer(socketserver.BaseRequestHandler):
 
   
         if self.path[-1] == '/':
-            
+            #how to get absolute path from https://docs.python.org/3/library/os.path.html#os.path.abspath (Copyright 2001-2022, Python Software Foundation)
             self.path = os.path.abspath(self.path) + '/'
             
-            #https://docs.python.org/3/library/os.path.html#os.path.abspath
             try:
                 self.updated_path = self.default_path + self.path + "index.html"
                 f = open(self.updated_path)
+                #how to use mimetype from https://docs.python.org/3/library/mimetypes.html( Copyright 2001-2022, Python Software Foundation)
                 self.content_type = mimetypes.guess_type(self.updated_path)
                 if self.code != 301:
                     self.code = 200
@@ -146,6 +135,7 @@ class MyWebServer(socketserver.BaseRequestHandler):
             try:
                 self.updated_path = self.default_path + self.path
                 f = open(self.updated_path)
+                #how to use mimetype from https://docs.python.org/3/library/mimetypes.html (Copyright 2001-2022, Python Software Foundation)
                 self.content_type = mimetypes.guess_type(self.updated_path)
                 self.code = 200
                 
@@ -163,21 +153,20 @@ class MyWebServer(socketserver.BaseRequestHandler):
         self.request.sendall(bytearray(output_data, 'utf-8'))
         
     def creating_headers(self):
-        #depending on code status, headers are sent
+        #depending on accepted code status, headers are sent
     
         self.message = None
         if self.code == 200:
             self.message = "OK"
             self.request.send(bytearray("{0} {1} {2}\r\n".format(self.protocol, self.code, self.message), 'utf-8'))
             self.request.send(bytearray("Server: Gurjog's Server\r\n", 'utf-8'))
-        
+            #how to print current GMT time https://www.askpython.com/python/examples/convert-seconds-hours-minutes (Copyright 2022 AskPython )
             datetime_string = time.strftime("%a, %d %b %Y %H:%M:%S GMT", time.gmtime(time.time()))
             self.request.send(bytearray("Date: {0}\r\n".format(datetime_string), 'utf-8'))
             self.request.send(bytearray("Connection: close \r\n", 'utf-8'))
             self.request.send(bytearray("Content-type: {0}\r\n".format(self.content_type[0]), 'utf-8'))
             self.request.send(bytearray("Content-Length: {0}\r\n".format(self.file_length), 'utf-8'))
-            #https://www.askpython.com/python/examples/convert-seconds-hours-minutes
-            #https://www.kite.com/python/answers/how-to-get-the-last-modified-time-of-a-file-in-python
+            #how to get last modified time https://www.kite.com/python/answers/how-to-get-the-last-modified-time-of-a-file-in-python (Copyright 2022 Kite)
             lastModified = time.strftime("%a, %d %b %Y %H:%M:%S GMT",(time.gmtime(os.path.getmtime(self.updated_path))))
             self.request.send(bytearray("Last-Modified: {0}\r\n\r\n".format(lastModified), 'utf-8'))
         
@@ -187,21 +176,21 @@ class MyWebServer(socketserver.BaseRequestHandler):
             self.message = "Moved Permanently"
             self.request.send(bytearray("{0} {1} {2}\r\n".format(self.protocol, self.code, self.message), 'utf-8'))
             self.request.send(bytearray("Server: Gurjog's server\r\n", 'utf-8'))
-        
+            #how to print current GMT time https://www.askpython.com/python/examples/convert-seconds-hours-minutes (Copyright 2022 AskPython )
             datetime_string = time.strftime("%a, %d %b %Y %H:%M:%S GMT", time.gmtime(time.time()))
             self.request.send(bytearray("Date: {0}\r\n".format(datetime_string), 'utf-8'))
             self.request.send(bytearray("Connection: close \r\n", 'utf-8'))
             self.request.send(bytearray("Content-type: {0}\r\n".format(self.content_type[0]), 'utf-8'))
             self.request.send(bytearray("Content-Length: {0}\r\n".format(self.file_length), 'utf-8'))
-            #https://www.askpython.com/python/examples/convert-seconds-hours-minutes
-            #https://www.kite.com/python/answers/how-to-get-the-last-modified-time-of-a-file-in-python
+            
+            #how to get last modified time https://www.kite.com/python/answers/how-to-get-the-last-modified-time-of-a-file-in-python (Copyright 2022 Kite)
             lastModified = time.strftime("%a, %d %b %Y %H:%M:%S GMT",(time.gmtime(os.path.getmtime(self.updated_path))))
             self.request.send(bytearray("Last-Modified: {0}\r\n".format(lastModified), 'utf-8'))
             self.request.send(bytearray("Location: {0}\r\n\r\n".format(BASEURL + self.path), 'utf-8'))
 
             
     def bad_request_error(self):
-        #format stolen from https://uofa-cmput404.github.io/cmput404-slides/04-HTTP.html#/20
+            #HTML format borrowed from https://uofa-cmput404.github.io/cmput404-slides/04-HTTP.html#/20 (Copyright 2014 Abram Hindle, Copyright 2019 Hazel Victoria Campbell and contributors)
             html_400_code = "<html>\r\n"\
                 "<head><title>400 BAD REQUEST</title></head>\r\n"\
                 "<body bgcolor=\"white\">\r\n"\
@@ -213,9 +202,8 @@ class MyWebServer(socketserver.BaseRequestHandler):
             self.message = "Bad Request"
             self.request.send(bytearray("{0} {1} {2}\r\n".format('HTTP/1.1', self.code, self.message), 'utf-8'))
             self.request.send(bytearray("Server: Gurjog's Server\r\n", 'utf-8'))
+            #how to print current GMT time https://www.askpython.com/python/examples/convert-seconds-hours-minutes (Copyright 2022 AskPython )
             datetime_string = time.strftime("%a, %d %b %Y %H:%M:%S GMT", time.gmtime(time.time()))
-           
-
             self.request.send(bytearray("Date: {0}\r\n".format(datetime_string), 'utf-8'))
             self.request.send(bytearray("Connection: close \r\n", 'utf-8'))
             self.request.send(bytearray("Content-type: text/html;charset=utf-8\r\n", 'utf-8'))
@@ -224,7 +212,7 @@ class MyWebServer(socketserver.BaseRequestHandler):
         
     def page_not_found_error(self):
 
-            #format stolen from https://uofa-cmput404.github.io/cmput404-slides/04-HTTP.html#/20
+            #HTML format borrowed from https://uofa-cmput404.github.io/cmput404-slides/04-HTTP.html#/20 (Copyright 2014 Abram Hindle, Copyright 2019 Hazel Victoria Campbell and contributors)
             html_404_code = "<html>\r\n"\
                 "<head><title>404 File Not Found</title></head>\r\n"\
                 "<body bgcolor=\"white\">\r\n"\
@@ -236,18 +224,13 @@ class MyWebServer(socketserver.BaseRequestHandler):
             self.message = "File not found"
             self.request.send(bytearray("{0} {1} {2}\r\n".format(self.protocol, self.code, self.message), 'utf-8'))
             self.request.send(bytearray("Server: Gurjog's Server\r\n", 'utf-8'))
+            #how to print current GMT time https://www.askpython.com/python/examples/convert-seconds-hours-minutes (Copyright 2022 AskPython )
             datetime_string = time.strftime("%a, %d %b %Y %H:%M:%S GMT", time.gmtime(time.time()))
-           
-
             self.request.send(bytearray("Date: {0}\r\n".format(datetime_string), 'utf-8'))
             self.request.send(bytearray("Connection: close \r\n", 'utf-8'))
             self.request.send(bytearray("Content-type: text/html;charset=utf-8\r\n", 'utf-8'))
             self.request.send(bytearray("Content-Length: {0}\r\n\r\n".format(len(html_404_code)), 'utf-8'))
             self.request.send(bytearray(html_404_code, 'utf-8'))
-
-    
-
-
 
 if __name__ == "__main__":
     connection = True
@@ -261,6 +244,4 @@ if __name__ == "__main__":
         server.serve_forever()
 
             
-#https://docs.python.org/3/library/socketserver.html#socketserver.BaseServer.finish_request
-
 
